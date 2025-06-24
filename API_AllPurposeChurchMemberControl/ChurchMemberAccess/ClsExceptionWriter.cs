1e29c4637bbc145150a104a8b6f44f5a1f1b66b4
Gary-Lu-IT <reimukaitoku@gmail.com>
2025-06-24 09:35:15 +0800

    API專案中加入錯誤紀錄函數

---
+++ API_AllPurposeChurchMemberControl/ChurchMemberAccess/ClsExceptionWriter.cs
@@ -0,0 +1,49 @@
+﻿using API_AllPurposeChurchMemberControl.SQLiteDB.ExceptionLogs;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace API_AllPurposeChurchMemberControl.ChurchMemberAccess
+{
+    /// <summary>例外狀況事件紀錄器</summary>
+    internal class ClsExceptionWriter
+    {
+        /// <summary>記錄錯誤訊息</summary>
+        /// <param name="message">文字錯誤訊息</param>
+        public static void WriteError(string message)
+        {
+            MethodBase? CallerOfThisFunc = new StackTrace().GetFrame(0).GetMethod();
+            using ExceptionLogContext elc = new();
+            int iMax = !elc.EXCEPTIONDATA.Any() ? (-2 ^ 31) : elc.EXCEPTIONDATA.Max(x => x.SERIAL) + 1;
+            elc.EXCEPTIONDATA.Add(new EXCEPTIONDATA
+            {
+                SERIAL = iMax,
+                EVENTTIME = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fffffff"),
+                EXMESSAGE = message,
+                EXSTACKTRACE = string.Empty,
+                CALLERFUNCTION = CallerOfThisFunc.Name,
+                PARAM = JsonSerializer.Serialize(CallerOfThisFunc.GetParameters())
+            });
+            elc.SaveChanges();
+        }
+        /// <summary>記錄錯誤訊息</summary>
+        /// <param name="exception">已發生例外狀況</param>
+        public static void WriteError(ref Exception exception)
+        {
+            MethodBase? CallerOfThisFunc = new StackTrace().GetFrame(0).GetMethod();
+            using ExceptionLogContext elc = new();
+            int iMax = !elc.EXCEPTIONDATA.Any() ? (-2 ^ 31) : elc.EXCEPTIONDATA.Max(x => x.SERIAL) + 1;
+            elc.EXCEPTIONDATA.Add(new EXCEPTIONDATA
+            {
+                SERIAL = iMax,
+                EVENTTIME = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fffffff"),
+                EXMESSAGE = exception.Message,
+                EXSTACKTRACE = exception.StackTrace ?? string.Empty,
+                CALLERFUNCTION = CallerOfThisFunc.Name,
+                PARAM = JsonSerializer.Serialize(CallerOfThisFunc.GetParameters())
+            });
+            elc.SaveChanges();
+        }
+    }
+}
--- API_AllPurposeChurchMemberControl/obj/Debug/net9.0/泛用基督教會會員管理系統API.AssemblyInfo.cs
+++ API_AllPurposeChurchMemberControl/obj/Debug/net9.0/泛用基督教會會員管理系統API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_AllPurposeChurchMemberControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3b77d91f462a203eb23f1a8d2354299e44930b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba165edfe02035e191fcb7332cdb5dd571a1e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_AllPurposeChurchMemberControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_AllPurposeChurchMemberControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
