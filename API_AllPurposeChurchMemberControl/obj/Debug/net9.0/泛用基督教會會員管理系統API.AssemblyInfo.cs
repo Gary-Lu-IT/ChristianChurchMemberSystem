362eb455bfafd8844913f5bba9b603501094be52
Gary-Lu-IT <reimukaitoku@gmail.com>
2025-06-29 22:01:41 +0800

    移除一些不重要的檔案

--- API_AllPurposeChurchMemberControl/obj/Debug/net9.0/泛用基督教會會員管理系統API.AssemblyInfo.cs
+++ API_AllPurposeChurchMemberControl/obj/Debug/net9.0/泛用基督教會會員管理系統API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_AllPurposeChurchMemberControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56eab0a04e3ffe9459646abf7692b8ac73dce422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a527ad0243f7858aa612d677e4f4931a09f346")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_AllPurposeChurchMemberControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_AllPurposeChurchMemberControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
