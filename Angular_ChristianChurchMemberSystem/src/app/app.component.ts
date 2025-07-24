7b76aa86006ec8bf3ac64b8566692f5425b515ba
Gary-Lu-IT <reimukaitoku@gmail.com>
2025-07-24 10:52:42 +0800

    Angular主頁面子選單切換

--- Angular_ChristianChurchMemberSystem/src/app/app.component.ts
+++ Angular_ChristianChurchMemberSystem/src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent {
   title = '泛用基督教會會員管理系統';
   userName = '使用者'; // 可根據登入狀態動態取得
   /** 教會會員管理下拉選單是否呈現*/
-  MemberMenu = false;
+  ShowMenu = "";
 
   logout() {
     // 登出邏輯
   }
   /** 切換教會會員管理下拉選單是否呈現*/
-  SwitchMemberMenu() {
-    this.MemberMenu = !this.MemberMenu;
+  SwitchShowMenu(sets:string) {
+    this.ShowMenu = sets;
   }
 }
